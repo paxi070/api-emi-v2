cd049049b99adcc201511673b80d6ef71ba765a5
pablov2 <paxi070@gmail.com>
2023-04-17 15:57:28 +0200

    ssss
    ss

--- function_updateConfirmado.php
+++ function_updateConfirmado.php
@@ -12,22 +12,20 @@ function updateParticipanteConfirmado($participanteInput, $participanteParams)
     }
     else
     {
-        return 'FALTA PARTICIPANTE ID 2';
-    }
-   
-    $participante_id = mysqli_real_escape_string($conn, $participanteParams['id']);
+        $participante_id = mysqli_real_escape_string($conn, $participanteParams['id']);
 
-    $query = "UPDATE participante SET confirmado = 1 WHERE id = '$participante_id' LIMIT 1" ;
-    $result = mysqli_query($conn, $query);
+        $query = "UPDATE participante SET confirmado = 1 WHERE id = '$participante_id' LIMIT 1" ;
+        $result = mysqli_query($conn, $query);
 
-    if($result) {
-        $data = [
-            'status' => 200,
-            'message' => 'Participante Confirmado'
-        ];
-        header("HTTP/1.0 200 OK");
-        return json_encode($data);       
-    }
+        if($result) {
+            $data = [
+                'status' => 200,
+                'message' => 'Participante Confirmado'
+            ];
+            header("HTTP/1.0 200 OK");
+            return json_encode($data);       
+        }
+    }  
 }
 
 ?>
