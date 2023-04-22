0ef83c2f3ac27318b8bfb172643aaa8b5df6f1b0
pablov2 <paxi070@gmail.com>
2023-04-22 17:19:44 +0200

    commit

---
+++ function_getParticipantesPorEvento.php
@@ -0,0 +1,36 @@
+<?php
+ 
+require 'conexion.php';
+
+function getParticipantesEventoId($eventoInput, $eventoParams)
+{
+    global $conn;
+
+    if(!isset($eventoParams['id']))
+    {
+        return 'FALTA EVENTO ID';
+    }
+    else
+    {
+        $evento_id = mysqli_real_escape_string($conn, $eventoParams['id']);
+
+        $query = "SELECT * FROM participante WHERE evento = '$evento_id'";
+        $query_run = mysqli_query($conn, $query);
+
+        if($query_run) {
+            if (mysqli_num_rows($query_run) > 0) {
+                $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
+
+                $data = [
+                    'status' => 200,
+                    'message' => 'Participantes Fetched',
+                    'data' => $res
+                ];
+                header("HTTP/1.0 200 OK");
+                return json_encode($data);
+            }
+        }
+    }  
+}
+
+?>
---
+++ participantes_evento.php
@@ -0,0 +1,27 @@
+<?php
+ 
+header('Access-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: GET');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+
+include('function_getParticipantesPorEvento.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if($requestMethod == 'GET'){
+
+    $inputData = json_decode(file_get_contents("php://input"), true);
+
+    if(empty($inputData))
+    {
+        $updateParticipante = getParticipantesEventoId($_POST, $_GET); 
+    }
+    else
+    {
+        $updateParticipante = getParticipantesEventoId($inputData, $_GET); 
+    }   
+
+    echo $updateParticipante;
+}
+?>
