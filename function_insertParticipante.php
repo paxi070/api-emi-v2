2aa596e6f087860ba4c855d8a27fb3e9d165dd66
pablov2 <paxi070@gmail.com>
2023-04-23 11:45:32 +0200

    CONTROL

--- function_insertParticipante.php
+++ function_insertParticipante.php
@@ -12,18 +12,30 @@ function insertParticipante($participanteInput, $participanteParams)
     $participante_country = mysqli_real_escape_string($conn, $participanteParams['country']);
     $participante_evento = mysqli_real_escape_string($conn, $participanteParams['evento']);
     $participante_confirmado = 1;
+    $participante_numero = 1;
 
-    $query = "INSERT INTO participante (nombre, categoria, email, country_long, evento, confirmado) VALUES ('$participante_aka', '$participante_categoria', '$participante_email', '$participante_country', '$participante_evento', '$participante_confirmado');" ;
-    $result = mysqli_query($conn, $query);
-
-    if($result) {
-        $data = [
-            'status' => 200,
-            'message' => 'Participante Insertado'
-        ];
-        header("HTTP/1.0 200 OK");
-        return json_encode($data);       
-    }   
+    if(!isset($participanteParams['aka']) ||
+    !isset($participanteParams['categoria']) ||
+    !isset($participanteParams['email']) ||
+    !isset($participanteParams['country']) ||
+    !isset($participanteParams['evento']))
+    {
+        return 'FALTA DATOS PARTICIPANTE';
+    }
+    else
+    {
+        $query = "INSERT INTO participante (nombre, categoria, email, country_long, evento, confirmado, numero) VALUES ('$participante_aka', '$participante_categoria', '$participante_email', '$participante_country', $participante_evento, $participante_confirmado, $participante_numero);" ;
+        $result = mysqli_query($conn, $query);
+    
+        if($result) {
+            $data = [
+                'status' => 200,
+                'message' => 'Participante Insertado'
+            ];
+            header("HTTP/1.0 200 OK");
+            return json_encode($data);       
+        }
+    }     
 }
 
 ?>
