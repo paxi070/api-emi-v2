4b66424194aa3021d28aeb5b69853df6cf3dafab
pablov2 <paxi070@gmail.com>
2023-05-31 11:53:02 +0200

    aqui

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
-const express = require('express');
-const app =  express();
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+    ".hbs",
+    exphbs.create({
+      defaultLayout: "main",
+      extname: ".hbs",
+    }).engine
+  );
+app.set("view engine", ".hbs");
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(require("./routes/index"));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", async (req, res) => {
+    
+    console.log('aquuiu');
+    res.send('recibido');
+  });
