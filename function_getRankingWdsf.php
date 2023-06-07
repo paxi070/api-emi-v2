d2f280c9420fab1e005b1c6db811d7fa8b3343b7
pablov2 <paxi070@gmail.com>
2023-06-07 13:55:38 +0200

    ddddd

--- function_getRankingWdsf.php
+++ function_getRankingWdsf.php
@@ -9,29 +9,21 @@ function rankingWdsf($participanteInput, $participanteParams)
     $participante_categoria = mysqli_real_escape_string($conn, $participanteParams['categoria']);
     $participante_evento = mysqli_real_escape_string($conn, $participanteParams['evento']);
 
-    if(!isset($participanteParams['categoria']) || !isset($participanteParams['evento']))
-    {
-        return 'FALTA DATOS';
-    }
-    else
-    {
-        $query = "SELECT participante, sum(notanumerica) FROM notaWDSF WHERE categoria = '$participante_categoria' and evento = '$participante_evento' group by participante_id_evento order by 2 desc";
-        //$query = "INSERT INTO participante (nombre, categoria, email, country_long, evento, confirmado, numero) VALUES ('$participante_aka', '$participante_categoria', '$participante_email', '$participante_country', $participante_evento, $participante_confirmado, $participante_numero);" ;
-        $query_run = mysqli_query($conn, $query);
+    $query = "SELECT participante, sum(notanumerica) FROM notaWDSF WHERE categoria = '$participante_categoria' and evento = '$participante_evento' group by participante_id_evento order by 2 desc";
+    $query_run = mysqli_query($conn, $query);
 
-        if($query_run) {
-            if (mysqli_num_rows($query_run) > 0) {
-                $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
+    if($query_run) {
+        if (mysqli_num_rows($query_run) > 0) {
+            $res = mysqli_fetch_all($query_run, MYSQLI_ASSOC);
 
-                $data = [
-                    'status' => 200,
-                    'message' => 'Ranking Fetched',
-                    'data' => $res
-                ];
-                header("HTTP/1.0 200 OK");
-                return json_encode($data);
-            }
-        } 
+            $data = [
+                'status' => 200,
+                'message' => 'Ranking Fetched',
+                'data' => $res
+            ];
+            header("HTTP/1.0 200 OK");
+            return json_encode($data);
+        }
     }
 
 ?>
